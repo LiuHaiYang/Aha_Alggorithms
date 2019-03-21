81f99ab00019a2edf08c92c7eabf38cb2f1ee9a3
LiuHaiYang <843800695@qq.com>
2019-03-21 22:46:17 +0800

    add BubbleSort

--- Code 1/Bucket sort/BuckerSort_2/BucketSort_2.cpp
+++ Code 1/Bucket sort/BuckerSort_2/BucketSort_2.cpp
@@ -8,14 +8,14 @@ int main()
 
 	scanf("%d",&n); // 输入一个数 n ， 表示接下来 有n个数
 
-	for(i=1;i<=n;i++)
+	for(i=1;i<=n;i++)  //循环读入n个数，并进行桶排序
 	{
-		scanf("%d",&t);
-		book[t]++;
+		scanf("%d",&t); //把每一个数读到变量t中
+		book[t]++; //进行计数，对编号为t的桶放一个数
 	}
 
-	for (i=1000;i>=0;i--)
-		for (j=1;j<=book[i];j++)
+	for (i=1000;i>=0;i--)   //依次判断编号 1000-0 的桶
+		for (j=1;j<=book[i];j++)  //出现了几次就将桶的编号打印几次
 			printf("%d ",i);
 
 	getchar(); getchar();
